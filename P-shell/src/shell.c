4da8a36343c34746d812bd3287384c8dac14aae0
PUNITH K only <punith34k@gmail.com>
2026-08-06 17:06:57 +0530

    Update shell.c

--- P-shell/src/shell.c
+++ P-shell/src/shell.c
@@ -1,1 +1,15 @@
-
+#include<stdio.h>
+#include<string.h>
+#include "shell.h"
+void shell_loop(){
+  char input[100];
+  while(true){
+   printf("pshell>");
+   fgets(input,sizeof(input),stdin);
+   input[strcspn(input,"\n")]=0;
+   if(strcmp(input,"exit")==0){
+     printf("i know u will come again ");
+     break; }
+    printf("you typed: %s\n",input);
+  }
+}
