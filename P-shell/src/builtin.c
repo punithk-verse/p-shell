21deb609dbc2a43e2ad7e62915082d07170828cf
punithk-verse <punith34k@gmail.com>
2026-08-06 13:30:57 +0000

    Add built in cd command

--- P-shell/src/builtin.c
+++ P-shell/src/builtin.c
@@ -1,1 +1,19 @@
 
+#include<stdio.h>
+#include<unistd.h>
+#include<string.h>
+
+void builtin_command(char **args){
+   
+    if(args[1]==NULL)
+    {
+        printf("Usage : cd<directory>\n");
+        return;
+    }
+    else{
+        if (chdir(args[1])!=0)
+        {
+            perror("chdir");
+        }
+    }
+}
--- P-shell/src/shell.c
+++ P-shell/src/shell.c
@@ -9,11 +9,22 @@ void shell_loop(){
    input[strcspn(input,"\n")]='\0';
    char *args[100];
    parse_command(input,args);
+  //  for(int i=0;args[i]!=NULL;i++)
+  //  {
+  //   printf("args[%d]=%s\n",i,args[i]);
+  //  }
    
-   if(strcmp(input,"exit")==0){
-     printf("i know u will come again ");
+   if(strcmp(args[0],"exit")==0){
+     printf("i know u will come again \n");
      break; 
     }
-    execute_command(args);
+    if(strcmp(args[0],"cd")==0)
+    {
+      builtin_command(args);
+    }
+    else
+    {
+      execute_command(args);
+    }
   }
 }
