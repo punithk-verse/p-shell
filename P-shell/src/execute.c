d522813b96df6f0b6ca9c6ab9ac780461aa1ce24
PUNITH K only <punith34k@gmail.com>
2026-08-06 17:11:05 +0530

    Add necessary headers to execute.c

--- P-shell/src/execute.c
+++ P-shell/src/execute.c
@@ -1,1 +1,5 @@
-
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h> //this gives special data types used by Os
+#include<sys/wait.h> //ethis give fun for controlling process 
+#include "shell.h"
