b55796dcfa48c5c70adf73a9428cc42f4bbeab55
PUNITH K only <punith34k@gmail.com>
2026-08-06 17:20:26 +0530

    Implement command execution in execute.c
    Added a function to execute shell commands using fork and execlp.

--- P-shell/src/execute.c
+++ P-shell/src/execute.c
@@ -3,3 +3,20 @@
 #include<sys/types.h> //this gives special data types used by Os
 #include<sys/wait.h> //ethis give fun for controlling process 
 #include "shell.h"
+
+void execute_command(char *command)
+{
+  pid_t pid = fork();
+ if (pid==0){
+   execlp(command,command,NULL);
+   printf("COMMAND NOT FOUND !);
+     }
+  else if(pid>0)
+     {
+     wait(NULL);
+     }
+  else
+     {
+    printf("fork failed \n");
+    }
+}
