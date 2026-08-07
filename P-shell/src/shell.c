12c7340e8439d184b40d394ae079c0b17b78b3ff
punithk-verse <punith34k@gmail.com>
2026-08-07 12:54:31 +0000

    Add dynamic shell prompt using getcwd (unistd.h)

--- P-shell/src/shell.c
+++ P-shell/src/shell.c
@@ -1,28 +1,33 @@
 #include<stdio.h>
 #include<string.h>
+#include<unistd.h>
 #include "shell.h"
 void shell_loop(){
   char input[100];
   while(1){
-   printf("pshell>");
+  char cwd[1024];
+  if(getcwd(cwd,sizeof(cwd))!=NULL)
+   {
+    printf("%s > ", cwd);
+    }
+  else
+    {
+    printf("pshell> ");
+    }
    fgets(input,sizeof(input),stdin);
    input[strcspn(input,"\n")]='\0';
    char *args[100];
    parse_command(input,args);
-  //  for(int i=0;args[i]!=NULL;i++)
-  //  {
-  //   printf("args[%d]=%s\n",i,args[i]);
-  //  }
+      if (args[0] == NULL)
+          {
+          continue;
+          }
    
    if(strcmp(args[0],"exit")==0){
      printf("i know u will come again \n");
      break; 
     }
-    if(strcmp(args[0],"cd")==0)
-    {
-      builtin_command(args);
-    }
-    else
+    if(!builtin_command(args))
     {
       execute_command(args);
     }
