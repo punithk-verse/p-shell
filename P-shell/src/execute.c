9d68d3e7a0b5888b3e19a56c45758a7665fb0c95
PUNITH K only <punith34k@gmail.com>
2026-09-25 11:44:41 +0530

    Implement output redirection in command execution
    Added support for output redirection in execute_command function.

--- P-shell/src/execute.c
+++ P-shell/src/execute.c
@@ -3,12 +3,14 @@
 #include<sys/types.h> //this gives special data types used by Os
 #include<sys/wait.h> //ethis give fun for controlling process 
 #include<string.h>
+#include<fcntl.h>
 #include<stdlib.h>
 #include "shell.h"
 
 void execute_command(char **args)
 {
   int pipe_index=-1;
+  
   for(int i=0;args[i]!=NULL;i++){
    if(strcmp(args[i],"|")==0)
    {
@@ -17,9 +19,36 @@ void execute_command(char **args)
    }
   }
   if(pipe_index==-1){
+    int redirect_index =-1;
+    for(int i=0;args[i]!=NULL;i++){
+      if(strcmp(args[i],">")==0){
+        redirect_index=i;
+        break;
+      }
+    }
    pid_t pid=fork();
 
   if(pid==0){
+    if(redirect_index!=-1){
+      if(args[redirect_index +1]==NULL){
+        fprintf(stderr,"pshell: missing output file \n");
+        exit(1);
+      }
+      int fd=open(args[redirect_index + 1],O_WRONLY | O_CREAT | O_TRUNC,0644);
+      if(fd==-1){
+        perror("open");
+        exit(1);
+      }
+      args[redirect_index]=NULL;
+      if(dup2(fd,STDOUT_FILENO)==-1){
+        perror("dup2");
+        close(fd);
+        exit(1);
+      }
+      trace_dup2(fd,STDOUT_FILENO);
+      close(fd);
+    }
+
    execvp(args[0],args);
    perror("execvp");
    exit(1);
